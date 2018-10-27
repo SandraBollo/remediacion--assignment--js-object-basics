f0a4b33b932fe6741f4ac7f349e28e7d1465e30e
SandraBollo <sandra.bollo.b@gmail.com>
2018-10-27 18:25:18 -0500

    first commit

---
+++ ex-06-createListOfObjects.js
@@ -0,0 +1,117 @@
+/**
+ * createListOfObjects()
+ *
+ * Write a function called createListOfObjects that accepts an
+ * an array of strings with first and last names and returns
+ * an array of objects that each have the property `firstName`
+ * and `lastName` and first name and last name values
+ * corresponding value
+ *
+ * var namesList = ['Cameron Betts', 'Shana Lopez', 'Angela Li']
+ *
+ * createListOfObjects(namesList)
+ *  =>
+ *  [
+ *     { firstName: 'Camer', lastName: 'Betts'},
+ *     { firstName: 'Shana', lastName: 'Lopez'},
+ *     { firstName: 'Angela', lastName: 'Li'}
+ *  ]
+ *
+ * HINT: You might be able to reuse some of the logic from createNameObject()
+*/
+
+
+// ++ YOUR CODE HERE
+
+function createListOfObjects(aRry){
+  let arrObjs = [ ]
+
+
+  for(var i = 0; i < aRry.length; i++){
+
+    let unArr = aRry[i].split(' ')
+
+    // console.log(unArr);
+
+    arrObjs.push({
+        firstName : unArr[0],
+        lastName : unArr[1]
+      })
+
+    // console.log( '**********');
+
+  }
+  console.log(arrObjs);
+  return arrObjs
+}
+
+
+
+
+
+
+
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+console.group('ex-06');
+  console.log('%cFunction: createListOfObjects', 'background-color: green; color: white')
+console.groupEnd();
+
+
+var realNinjas = [
+  'Chuck Norris',
+  'Jackie Chan',
+  'Lucy Liu',
+  'Billy Blanks',
+  'Michelle Yeoh',
+  'Jet Li'
+]
+
+var realSportsStars = [
+	'Kenny Powers',
+	'Ricky Vaughn',
+	'Dottie Hinson',
+	'Jesus Shuttlesworth',
+	'Scotty Smalls'
+]
+
+
+var ninjaListOfObjects = createListOfObjects(realNinjas)
+var sportStarsListOfObjects = createListOfObjects(realSportsStars)
+
+/* ----------------------- TEST-1  ----------------------- */
+// Function returns an array of objects
+/* ------------------------------------------------------ */
+console.log('TEST-1');
+
+console.assert(Array.isArray(ninjaListOfObjects) === true)
+console.assert(typeof ninjaListOfObjects[0] === "object")
+
+
+//* ----------------------- TEST-2  ----------------------- */
+// Each object element of array has `firstName` + `lastName`
+//    properties
+/* ------------------------------------------------------ */
+console.log('TEST-2');
+
+// createListOfObjects(realNinjas)
+console.assert(ninjaListOfObjects[0].firstName === "Chuck")
+console.assert(ninjaListOfObjects[0].lastName === "Norris")
+
+console.assert(ninjaListOfObjects[1].firstName === "Jackie")
+console.assert(ninjaListOfObjects[1].lastName === "Chan")
+
+console.assert(ninjaListOfObjects[3].firstName === "Billy")
+console.assert(ninjaListOfObjects[3].lastName === "Blanks")
+
+
+
+// createListOfObjects(realSportsStars)
+console.assert(sportStarsListOfObjects[1].firstName === "Ricky")
+console.assert(sportStarsListOfObjects[1].lastName === "Vaughn")
+
+console.assert(sportStarsListOfObjects[3].firstName === "Jesus")
+console.assert(sportStarsListOfObjects[3].lastName === "Shuttlesworth")
